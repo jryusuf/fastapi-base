4dd6819cbde09560ca82ce6e5a99dd7da18672d5
Yusuf Yuksel <yusufyuksel@aol.com>
2024-11-04 18:10:28 +0000

    fastapi unittest example and library setup

--- app/tests/test_eaxmple.py
+++ app/tests/test_eaxmple.py
@@ -1,2 +1,14 @@
-def test_additioin():
-    assert 1 + 1 == 2
+import pytest
+
+from httpx import AsyncClient
+from fastapi import FastAPI
+
+from app.main import app
+
+
+@pytest.mark.asyncio
+async def test_read_main():
+    async with AsyncClient(app=app, base_url="http://localhost:8000") as client:
+        response = await client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"Hello": "World"}
