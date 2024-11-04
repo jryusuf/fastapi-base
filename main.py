ae4ff818f2352279890b948f2c67c28c87f2b567
Yusuf Yuksel <yusufyuksel@aol.com>
2024-11-04 15:45:27 +0000

    test pre-commit linting

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+def example_function(x, y):
+    result = x + y
+    print("The result is:", result)
+    return result
+
+
+def unused_function():
+    pass
